7d9c1aa886f9b2193cb40393da8e81b0c515cc63
Tim Anema <tim.anema@netlify.com>
2024-11-20 12:33:13 -0500

    Fix

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"context"
 	"github.com/tanema/helloWorld/lib/netlify"
+	"net/http"
 )
 
-func handler(ctx context.Context, req netlify.Request) *netlify.Response {
+func handler(ctx context.Context, req *http.Request) *netlify.Response {
 	return netlify.Text("Hello Netlify")
 }
 
